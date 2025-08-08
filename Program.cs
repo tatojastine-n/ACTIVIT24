7c97dffcd1c445835938ceb19c82394388766f62
tatojastine-n <jajanicoletato@gmail.com>
2025-08-08 21:59:47 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAuthenticationLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string validUsername = "admin";
+            const string validPassword = "1234";
+            bool isAuthenticated = false;
+
+            while (!isAuthenticated)
+            {
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+
+                if (username == validUsername && password == validPassword)
+                {
+                    isAuthenticated = true;
+                    Console.WriteLine("\nLogin successful!\n");
+                }
+                else
+                {
+                    Console.WriteLine("\nTry again. Incorrect credentials.\n");
+                }
+            }
+
+        }
+    }
+}
